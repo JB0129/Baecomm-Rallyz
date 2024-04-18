e892d7c42e6fafc5f5286c57f8b075eac5a1a294
JB0129 <rltkqja1@gmail.com>
2024-04-18 22:04:40 +0900

    기본 기능 구현 완료

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,250 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import { ReactComponent as LeftArrow } from "../assets/images/LeftArrow.svg";
+import { ReactComponent as RightArrow } from "../assets/images/RightArrow.svg";
+
+type ProductInfo = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
 
 const ProductDetail: React.FC = () => {
-  return <div>ProductDetail</div>;
+  const navigate = useNavigate();
+  const { productId } = useParams(); // 제품 ID
+  const imagesRef = useRef<HTMLUListElement>(null); // images DOM
+  const [media, setMedia] = useState<number>(0); // images 개수
+  const [productInfo, setProductInfo] = useState<ProductInfo>(); // 상품 정보
+
+  // 상품 정보 API
+  const getProductInfo = () => {
+    return fetch(`https://dummyjson.com/products/${productId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProductInfo(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // 상품 정보 가져오기
+  useEffect(() => {
+    getProductInfo();
+  }, []);
+
+  // 이미지 왼쪽 스크롤
+  const handleLeftScroll = () => {
+    if (media > 0 && imagesRef.current) {
+      imagesRef.current.style.transform = `translateX(${-260 * (media - 1)}px)`;
+      setMedia((media) => media - 1);
+    }
+  };
+
+  // 이미지 오른쪽 스크롤
+  const handleRightScroll = () => {
+    if (
+      productInfo &&
+      media < productInfo.images.length - 1 &&
+      imagesRef.current
+    ) {
+      imagesRef.current.style.transform = `translateX(${-260 * (media + 1)}px)`;
+      setMedia((media) => media + 1);
+    }
+  };
+
+  return (
+    <DetailContainer>
+      <BackButton
+        onClick={() => {
+          navigate(`/`);
+          sessionStorage.setItem("itemId", String(productId));
+        }}
+      >
+        목록으로 돌아가기
+      </BackButton>
+      {productInfo ? (
+        <DetailBox>
+          <DetailThumbnail
+            src={productInfo.thumbnail}
+            alt="상품 썸네일 이미지"
+          />
+          <DetailMedia>
+            <LeftScroll onClick={handleLeftScroll} />
+            <DetailImgs>
+              <DetailImgBox ref={imagesRef}>
+                {productInfo.images.map((image, idx) => (
+                  <DetailImg key={idx}>
+                    <OtherImg src={image} alt={`${idx}번 상품 이미지`} />
+                  </DetailImg>
+                ))}
+              </DetailImgBox>
+            </DetailImgs>
+            <RightScroll onClick={handleRightScroll} />
+          </DetailMedia>
+          <DetailPrice>{`$${productInfo.price.toLocaleString()}`}</DetailPrice>
+          <DetailInfo>
+            <DetailBrand>{`[${productInfo.brand}]`}</DetailBrand>
+            <DetailTitle>{productInfo.title}</DetailTitle>
+          </DetailInfo>
+          <DetailDesc>{productInfo.description}</DetailDesc>
+        </DetailBox>
+      ) : (
+        <EmtpyBox>해당 제품 정보가 없습니다.</EmtpyBox>
+      )}
+    </DetailContainer>
+  );
 };
 
 export default ProductDetail;
+
+export const DetailContainer = styled.article`
+  position: relative;
+  background-color: white;
+  border-radius: 12px;
+  width: 100%;
+  max-width: 1170px;
+  min-height: 100vh;
+  margin: 25px 0px;
+  padding: 50px 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+`;
+
+export const BackButton = styled.button`
+  position: absolute;
+  top: 10px;
+  left: 0;
+  width: 200px;
+  height: 40px;
+  font-weight: 600;
+  font-size: 15px;
+  &:hover {
+    color: rgb(253, 196, 149);
+  }
+  &:active {
+    color: rgb(255, 208, 169);
+  }
+`;
+export const DetailBox = styled.div`
+  width: 100%;
+  padding: 10px 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+`;
+export const DetailThumbnail = styled.img`
+  border-radius: 12px;
+  width: 100%;
+  max-width: 700px;
+  height: 400px;
+`;
+export const DetailMedia = styled.section`
+  position: relative;
+  width: 100%;
+  max-width: 800px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const DetailImgs = styled.div`
+  width: 100%;
+  max-width: 700px;
+  overflow: hidden;
+`;
+export const DetailImgBox = styled.ul`
+  width: 100%;
+  max-width: 700px;
+  padding: 10px 0px;
+  transition: 300ms;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+export const DetailImg = styled.li`
+  margin-right: 10px;
+`;
+export const OtherImg = styled.img`
+  width: 250px;
+  height: 150px;
+  border-radius: 12px;
+`;
+export const LeftScroll = styled(LeftArrow)`
+  position: absolute;
+  top: 70px;
+  left: 10px;
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  path {
+    fill: rgb(95, 95, 95);
+    &:hover {
+      fill: rgb(115, 115, 115);
+    }
+    &:active {
+      fill: rgb(105, 105, 105);
+    }
+  }
+`;
+export const RightScroll = styled(RightArrow)`
+  position: absolute;
+  top: 70px;
+  right: 10px;
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  path {
+    fill: rgb(95, 95, 95);
+    &:hover {
+      fill: rgb(115, 115, 115);
+    }
+    &:active {
+      fill: rgb(105, 105, 105);
+    }
+  }
+`;
+export const DetailInfo = styled.div`
+  width: 100%;
+  max-width: 700px;
+  margin: 5px 0px;
+  margin: 15px 0px 30px 0px;
+  padding: 0px 10px;
+  color: rgb(82, 82, 82);
+  font-size: 25px;
+  font-weight: 600;
+  white-space: nowrap;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+export const DetailBrand = styled.span`
+  margin-right: 5px;
+`;
+export const DetailTitle = styled.span``;
+export const DetailPrice = styled.div`
+  width: 100%;
+  max-width: 700px;
+  margin: 10px 0px;
+  padding: 0px 20px;
+  text-align: end;
+  font-size: 25px;
+  font-weight: 600;
+`;
+export const DetailDesc = styled.div`
+  width: 100%;
+  max-width: 700px;
+  height: 150px;
+  color: rgb(82, 82, 82);
+  font-weight: 600;
+`;
+export const EmtpyBox = styled.section``;
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,7 +1,294 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styled from "styled-components";
+
+type productType = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
+const ROOT_URL = `https://dummyjson.com`;
 
 const ProductList: React.FC = () => {
-  return <div>ProductList</div>;
+  const navigate = useNavigate();
+  const [productLists, setProductLists] = useState([]); // 상품 리스트
+  const [total, setTotal] = useState<number>(0); // 상품 전체 개수
+  const [limit, setLimit] = useState<number>(10); // 상품 출력 개수
+  const [skip, setSkip] = useState<number>(0); // 상품 스킵 개수
+
+  // input value 변경 함수
+  const [inputValue, setInputValue] = useState<string>(""); // 검색 input value
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputValue(e.target.value);
+
+  // 쿼리 스트링 값 설정
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get("q");
+
+  // 전체 상품 조회
+  const itemId = sessionStorage.getItem("itemId");
+  const getProducts = () => {
+    const url = !keyword
+      ? `${ROOT_URL}/products?limit=${
+          Math.ceil(Number(itemId) / 10) * 10 || limit
+        }&skip=${skip}`
+      : `${ROOT_URL}/products/search?q=${keyword}&limit=${
+          Math.ceil(Number(itemId) / 10) * 10 || limit
+        }&skip=${skip}`;
+    return fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setProductLists(data.products);
+        setTotal(data.total);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // 특정 상품 조회
+  const getMoreProducts = () => {
+    const url = !keyword
+      ? `${ROOT_URL}/products?limit=${limit}&skip=${skip + 10}`
+      : `${ROOT_URL}/products/search?q=${keyword}&limit=${limit}&skip=${
+          skip + 10
+        }`;
+    return fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setProductLists((productLists) => productLists.concat(data.products));
+        setTotal(data.total);
+        setSkip((skip) => skip + 10);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // 상품 검색
+  const setSortParams = () => {
+    searchParams.set("q", inputValue);
+    setSearchParams(searchParams);
+  };
+
+  // 검색 이벤트
+  const handleSearch = () => {
+    if (!inputValue) return alert("검색할 단어를 입력해주세요.");
+    setSortParams();
+  };
+
+  // Enter 입력 시, 상품 검색하기
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  // 상품 리스트 가져오기
+  useEffect(() => {
+    getProducts();
+    return () => {
+      setInputValue("");
+    };
+  }, [keyword]);
+
+  // 전에 선택한 상품으로 이동
+  useEffect(() => {
+    if (itemId && productLists.length) {
+      const selectedListElement = document.getElementById(itemId);
+      if (selectedListElement) {
+        selectedListElement.scrollIntoView({ behavior: "auto" });
+        sessionStorage.removeItem("itemId"); // 이동 후 초기화
+      }
+    }
+  }, [productLists]);
+
+  return (
+    <ProductContainer>
+      <SearchBox>
+        <SearchInput
+          placeholder="검색어를 입력해주세요."
+          value={inputValue}
+          onChange={handleChangeInput}
+          onKeyDown={handleEnter} // up을 할지 down을 할지?
+        />
+        <SearchButton onClick={handleSearch}>검색</SearchButton>
+      </SearchBox>
+      <MsgBox>{keyword && `"${keyword}"로 검색한 결과입니다.`}</MsgBox>
+      {productLists.length > 0 ? (
+        <ProductItems>
+          {productLists.map((item: productType) => (
+            <ProductItem
+              key={item.id}
+              id={String(item.id)}
+              onClick={() => navigate(`/product/${item.id}`)}
+            >
+              <ProductImg src={item.thumbnail} alt="상품 썸네일 이미지" />
+              <ProductInfo>
+                <ProductBrand className="productInfo">
+                  {`[${item.brand}]`}
+                </ProductBrand>
+                <ProductTitle className="productInfo">
+                  {item.title}
+                </ProductTitle>
+              </ProductInfo>
+              <ProductPrice>{`$${item.price.toLocaleString()}`}</ProductPrice>
+            </ProductItem>
+          ))}
+          {productLists.length < total && (
+            <MoreButton onClick={getMoreProducts}>더보기</MoreButton>
+          )}
+        </ProductItems>
+      ) : (
+        <EmptyLists>
+          <span>존재하는 상품이 없습니다.</span>
+        </EmptyLists>
+      )}
+    </ProductContainer>
+  );
 };
 
 export default ProductList;
+
+export const ProductContainer = styled.article`
+  width: 100%;
+  padding: 10px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+`;
+export const SearchBox = styled.section`
+  margin: 20px 0px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+export const SearchInput = styled.input`
+  border: 3px solid rgb(120, 120, 120);
+  border-radius: 6px;
+  width: 400px;
+  height: 50px;
+  margin-right: 10px;
+  padding-left: 15px;
+  font-size: 15px;
+`;
+export const SearchButton = styled.button`
+  background-color: rgb(95, 95, 95);
+  width: 80px;
+  height: 50px;
+  border-radius: 6px;
+  color: white;
+  font-weight: 600;
+  font-size: 15px;
+  &:hover {
+    background-color: rgb(115, 115, 115);
+  }
+  &:active {
+    background-color: rgb(105, 105, 105);
+  }
+`;
+export const MsgBox = styled.div`
+  height: 14px;
+  margin: 0px 0px 20px 0px;
+  color: rgb(66, 66, 66);
+  font-size: 14px;
+  font-weight: 600;
+`;
+export const ProductItems = styled.ul`
+  background-color: rgb(255, 255, 255);
+  border-radius: 8px;
+  padding: 30px 50px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  row-gap: 20px;
+  column-gap: 40px;
+  place-items: start center;
+`;
+export const ProductItem = styled.li`
+  background-color: rgb(244, 244, 244);
+  border-radius: 8px;
+  width: 100%;
+  max-width: 380px;
+  padding: 20px;
+  margin-bottom: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  &:hover {
+    background-color: rgb(238, 238, 238);
+
+    .productInfo {
+      color: blue;
+    }
+  }
+`;
+export const ProductImg = styled.img`
+  border-radius: 8px;
+  width: 100%;
+  height: 210px;
+  margin: 0px 0px 5px 0px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+export const ProductInfo = styled.div`
+  width: 100%;
+  margin: 5px 0px;
+  padding: 0px 5px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  color: rgb(82, 82, 82);
+  font-weight: 600;
+`;
+export const ProductBrand = styled.span`
+  padding: 5px 0px;
+  white-space: nowrap;
+  margin-right: 5px;
+`;
+export const ProductTitle = styled.span`
+  padding: 5px 0px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+export const ProductPrice = styled.div`
+  width: 100%;
+  margin: 5px 0px;
+  padding: 0px 10px;
+  text-align: end;
+  font-weight: 600;
+`;
+export const MoreButton = styled.button`
+  grid-column: 1 / 3;
+  background-color: rgb(95, 95, 95);
+  width: 200px;
+  height: 40px;
+  border-radius: 6px;
+  color: white;
+  font-weight: 600;
+  font-size: 15px;
+  &:hover {
+    background-color: rgb(115, 115, 115);
+  }
+  &:active {
+    background-color: rgb(105, 105, 105);
+  }
+`;
+export const EmptyLists = styled.section`
+  background-color: rgb(255, 255, 255);
+  border-radius: 8px;
+  width: 100%;
+  max-width: 900px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
